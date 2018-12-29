35cd2b7224d9afd980ca7b34bc68b4d40fc9aff9
Enrique Chaviano <chg.enrique@gmail.com>
2018-12-29 01:37:49 -0500

    Project configuration

--- config/app.php
+++ config/app.php
@@ -175,6 +175,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Coders service providers
+         */
+        Reliese\Coders\CodersServiceProvider::class,
     ],
 
     /*
